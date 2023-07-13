05c68260cde98d218535f5a6a8e29b930583b3f0
hellun205 <hellun515@gmail.com>
2023-07-13 16:45:39 +0900

    option panel

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,13 +1,18 @@
-﻿using Cinema;
+﻿using System;
+using Cinema;
 using Load;
 using Load.Scenes;
 using ScreenEffect;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Manager
 {
   public class GameManager : SingleTon<GameManager>, IDontDestroy
   {
+    [SerializeField]
+    private GameObject optionPanel;
+    
     public void Die(string retryScene)
     {
       SceneLoader.Instance.Load("Die", null, null, () => FindObjectOfType<Die>().retryScene = retryScene);
@@ -26,5 +31,17 @@ namespace Manager
         ScreenEffects.FadeOut,
         ScreenEffects.FadeIn
       );
+
+    public void ToggleOptionPanel()
+    {
+      Time.timeScale = optionPanel.activeSelf ? 1f : 0f;
+      optionPanel.SetActive(!optionPanel.activeSelf);
+    }
+
+    private void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.Escape))
+        ToggleOptionPanel();
+    }
   }
 }
